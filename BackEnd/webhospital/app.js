b4cf1968900bf7b32fcb90ca12cdb89afce75693
tr0b <trobin3000600@gmail.com>
2020-03-01 23:24:46 -0600

    Added MongoDB Connection String to Database

--- BackEnd/webhospital/app.js
+++ BackEnd/webhospital/app.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const port = 3000;
+//Establish MongoDB Connection
+
+require("./db/database");
 
 app.get("/", (req, res) => res.send("Web Hospital!"));
 
---
+++ BackEnd/webhospital/db/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// Mongo Default Values
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useCreateIndex", true);
+
+//Mongo DB Connection
+mongoose.connect(process.env.MONGO_CLOUD_URL)
+	.then(db => console.log("InstHealth MongoDB Connected!"))
+	.catch(error => console.error(error));
