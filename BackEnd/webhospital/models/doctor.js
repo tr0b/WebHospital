a7b5a36219837bbb6a6de2e504ff1184bcacf6e0
tr0b <trobin3000600@gmail.com>
2020-03-03 16:49:46 -0600

    Fix in the doctor model

--- BackEnd/webhospital/models/doctor.js
+++ BackEnd/webhospital/models/doctor.js
@@ -5,10 +5,10 @@ const { Schema } = mongoose;
 const DoctorSchema = new Schema({
 	name: { type: String, required: true },
 	last_name: { type: String, required: true },
-	idCard:{type:String, required: true},
+	idCard: { type: String, required: true },
 	isActive: { type: Boolean, required: true, default: true },
-	birthDate:{type:Date, required: true},
-	gender:{type:String, required: true},
+	birthDate: { type: Date, required: true },
+	gender: { type: String, required: true },
 	specialty: { type: String, required: true },
 	address: {
 		canton: { type: mongoose.Schema.Types.ObjectId, ref: "Canton" }
